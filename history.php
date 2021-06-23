666e7588a4986a3bc6b7ea2dda60665252ab7e35
Daniel Fernández-Pacheco Santamaría <55846085+DanielFernandezPacheco@users.noreply.github.com>
2021-06-23 23:44:57 +0200

    favicon

--- history.php
+++ history.php
@@ -32,7 +32,8 @@ if (isset($_POST['numSongs'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Document</title>
+    <link rel="icon" href="images/SpotHistory.svg">
+    <title>SpotHistory - Historial</title>
 </head>
 
 <body>
--- spotHistory.php
+++ spotHistory.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-
-    <title>Últimas escuchas</title>
+    <link rel="icon" href="images/SpotHistory.svg">
+    <title>SpotHistory</title>
 </head>
 
 <body>
