fa5edef9817c4cf0c7c469cbf2cada8c32f686bd
Daniel Fernández-Pacheco Santamaría <danifpsan@gmail.com>
2022-02-04 21:38:12 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@ set_include_path(__DIR__);
 require_once 'vendor/autoload.php';
 
 //complete from Spotify Developer Dashboard, specific for each app
-$clientID = "2ce58ab3145b4aee99deec1cea21baa0";
-$clientSecret ="a555d8d38f484b51b2e0e7954b885dc9";
+$clientID = "";
+$clientSecret ="";
 
 //replace with the base uri of the project without
 $base_uri = "http://localhost/SpotHistory";
